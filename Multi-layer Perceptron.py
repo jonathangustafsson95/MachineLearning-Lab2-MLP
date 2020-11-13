8425a7304ab80531b2c879c796b5932fd636ad3f
challe <im.challe@gmail.com>
2020-11-13 16:31:36 +0100

    final this friday

--- Multi-layer Perceptron.py
+++ Multi-layer Perceptron.py
@@ -1,4 +1,6 @@
 from Layer import Layer
+from Loss import SquaredErrorLoss
+from Activation import InputActivationFunction, SigmoidActivationFunction, LinearActivationFunction
 
 class MLP:
     def __init__(self, loss_function):
@@ -18,7 +20,39 @@ class MLP:
         pass
 
     def train(self, x, y, learning_rate=0.01, n_epochs=100):
-        pass
+        samples = len(x)
+
+        for i in range(n_epochs):
+            err = 0
+            
+            for j in range(len(samples)):
+
+                output = x[j]
+                for k, layer in enumerate(self.layers):
+                    input_data = output
+                    output = []
+
+                    for node in range(layer.n_nodes):
+                        node_value = layer.forward(input_data)
+                        output.append(self.activations_functions[k](node_value))
+
+                err += self.loss_function(output ,y[j]) # display purpose
+                
+                # fault + backprop
+                    
+
 
     def predict(self, x, y):
         pass
+
+
+l = [ 
+    [1,2,3,4,5],
+    [1,2,3,4,5],
+    [1,2,3,4,5]
+    ]
+
+net = MLP(SquaredErrorLoss)
+net.add_layer(len(l[0]), InputActivationFunction)
+net.add_layer(8, SigmoidActivationFunction)
+net.add_layer(2, LinearActivationFunction)

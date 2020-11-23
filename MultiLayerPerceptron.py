2d25bd86bb184a23b769aca612095cdfcf1d5d82
Jonathan Gustafsson <jonathangustafsson95@gmail.com>
2020-11-23 16:31:11 +0100

    predictions

--- MultiLayerPerceptron.py
+++ MultiLayerPerceptron.py
@@ -2,12 +2,16 @@ from Layer import Layer
 from Loss import SquaredErrorLoss
 from Activation import InputActivationFunction, SigmoidActivationFunction, LinearActivationFunction
 import numpy as np
+from Normalizer import Normalizer
+
 
 class MLP:
     def __init__(self, loss_function):
         self.layers = []
         self.activations_functions = []
         self.loss_function = loss_function
+        self.normalizer = Normalizer()
+
 
     def add_layer(self, size, activation_function):
         n_inputs = size
@@ -17,46 +21,52 @@ class MLP:
         self.layers.append(Layer(size, n_inputs))
         self.activations_functions.append(activation_function)
 
-    def _backprop(self, x, y, d_loss, learning_rate):
+    def _backprop(self, x, d_loss, learning_rate):
         
         for i in range(len(self.layers)-1, 1, -1):
             d_loss = self.activations_functions[i].backward(x).reshape(len(x), -1) * d_loss
             x = self.layers[i].backprop(d_loss, learning_rate)
 
-    def train(self, x, y, learning_rate=0.01, n_epochs=1000):
+    def train(self, x, y, learning_rate=0.01, n_epochs=10):
+
+        # Normalize data
+        self.normalizer.fit(x,y)
+        x, y = self.normalizer.normalize(x,y)
+        print("Data after norm:")
+        print("X: {}, Y: {}" .format(x,y))
+
         for i in range(n_epochs): 
             sum_error = 0
             output = x
             for j, layer in enumerate(self.layers):
                 output = self.activations_functions[j].forward(layer.forward(output))
-
-            #print('output after actv func')
-            #print(output)
-
-            # print(np.average(self.loss_function.forward(output, y)))
-
-            #print("Y: {}".format(y))
-            #print("Y shape: {}".format(y.shape))
-
-
-            error = y - output
-            #print("Error: {}".format(error))
-
+            
             sum_error += (np.average(self.loss_function.forward(output, y)))
-            #print("Sum Error: {}".format(sum_error))
-
 
             print("Error: {} at epoch {}".format(sum_error / len(x), i+1))
-            
-            
+                        
             d_loss = self.loss_function.backward(output, y)
 
-            self._backprop(output, y, d_loss, learning_rate)
+            self._backprop(output, d_loss, learning_rate)
+
+        print(output)
+        out = self.normalizer.renormalize(output)
+        print(out)
+
+        yNorm = self.normalizer.renormalize(y)
+        print(yNorm)
+
+        print(np.average(yNorm))
+        print(np.average(out))
 
     def predict(self, x, y): 
-        pass
-        
+        # Normalize data
+        x, y = self.normalizer.normalize(x, y)
 
-input = np.array([[0,0], [0,1], [1,0],[1,1]])
-target = np.array([[0,1,1,1]])
-# Reshape .reshape(4,1)
+        output = x
+        for j, layer in enumerate(self.layers):
+            output = self.activations_functions[j].forward(layer.forward(output))
+        
+        return self.normalizer.renormalize(output)
+
+        
--- main.py
+++ main.py
@@ -14,11 +14,9 @@ def main():
     y = y[:len(y)]
     y = y.reshape(len(x),1)
 
-    # Normalize data
-    normalizer = Normalizer()
-    normalizer.fit(x,y)
-    x, y = normalizer.normalize(x,y)
+    print("Data before norm:")
     print("X: {}, Y: {}" .format(x,y))
+
 
     # Define loss function and activation function
     loss_func = SquaredErrorLoss()
@@ -32,17 +30,30 @@ def main():
     # Input layer
     model.add_layer(2, activation_func_inp)
     # Hidden layer
-    model.add_layer(4, activation_func_hid)
-    # Hidden layer
-    model.add_layer(2, activation_func_hid)
-    # Hidden layer
     model.add_layer(2, activation_func_hid)
     # Output
     model.add_layer(1, activation_func_out)
 
     # Train
-    model.train(x, y, 0.02, 1000)
+    model.train(x, y, 0.01, 500)
 
+    x1 = data[:, :-1]
+    y1 = data[:, -1]
+    x1 = x1[:50]
+    y1 = y1[:50]
+    y1 = y1.reshape(len(x1),1)
+
+    print("X1:  {}  \nY1: {}".format(x1,y1))
+    # Make predict
+    pred = model.predict(x1, y1)
+
+    print("\n \n Network pred:\n {}".format(pred))
+
+    print("Avg TARGET")
+    print(np.average(y1))
+
+    print("Avg PRED")
+    print(np.average(pred))
 
 if __name__ == "__main__":
     main()
